dd6371558435e548979d97b871cc6d8218a1e917
Mario <dejadme_el_silencio@hotmail.com>
2026-03-18 20:22:49 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { StatusBar } from '@capacitor/status-bar';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  imports: [IonApp, IonRouterOutlet],
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  async ngOnInit() {
+    await StatusBar.setOverlaysWebView({ overlay: false });
+  }
+}
