68537b7725a7083c3af43fbd7cedfcf7c98ef8c7
Mario <dejadme_el_silencio@hotmail.com>
2026-03-18 20:56:18 +0100

    add insetsHandling

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,11 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'demo-safe-area',
   webDir: 'www',
+  plugins: {
+    SystemBars: {
+      insetsHandling: 'css',
+    },
+  },
 };
 
 export default config;
